168a8781d662d5de392a57b2af33eb2461b224ef
forsigner <chenzhiyi@chenzhiyideiMac.local>
2015-12-30 17:24:46 +0800

    refactor

--- src/js/init.js
+++ src/js/init.js
@@ -1,4 +1,4 @@
-let instance = require('./instance');
+let Instance = require('./instance');
 let clickRail = require('./event/click-rail');
 let dragSlider = require('./event/drag-slider');
 let mouseWheel = require('./event/mouse-wheel');
@@ -7,7 +7,7 @@ let pressKeyboard = require('./event/press-keyboard');
 module.exports = function(element) {
   var $content = element.firstElementChild;
   if ($content.scrollHeight > $content.clientHeight) {
-    let i = new instance(element);
+    let i = new Instance(element);
 
     clickRail(i);
     dragSlider(i);
--- src/js/instance.js
+++ src/js/instance.js
@@ -64,5 +64,4 @@ class Instance {
 
 }
 
-
 module.exports = Instance;
