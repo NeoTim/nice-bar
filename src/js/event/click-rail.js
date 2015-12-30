f329ebb4d7a6b11d111798730173578011daca8a
forsigner <chenzhiyi@chenzhiyideiMac.local>
2015-12-30 20:39:03 +0800

    add comment

--- src/js/event/click-rail.js
+++ src/js/event/click-rail.js
@@ -5,40 +5,71 @@ let dom = require('../util/dom');
 
 module.exports = function(i) {
 
-  function getNewTop(layerY) {
-    let newTop = layerY - i.sliderY.height / 2;
-    if (newTop < 0) {
+  /**
+   * get slider's new top
+   * @param  {number} offsetY  a property from mouseEvent
+   * @return {number} newTop slider's new top
+   */
+  function getNewTop(offsetY) {
+    let newTop;
+
+    if (offsetY < i.sliderY.height / 2) {
       newTop = 0;
-    } else if (newTop + i.sliderY.height > i.railY.height) {
+    } else if (offsetY > i.railY.height - i.sliderY.height / 2) {
       newTop = i.railY.height - i.sliderY.height;
+    } else {
+      newTop = offsetY - i.sliderY.height / 2;
     }
 
     return newTop;
   }
 
-  function updateBox(newTop, originTop) {
+  /**
+   * update content box scrollTop
+   * @param  {number} newTop      slider's newTop
+   * @param  {number} originTop   slider's originTop
+   * @return null
+   */
+  function updateContent(newTop, originTop) {
     let journey = newTop - originTop;
     let scrollTop = journey / i.ratioY;
-    i.box.element.scrollTop += scrollTop;
+    i.content.element.scrollTop += scrollTop;
   }
 
+  /**
+   * update slider's css top
+   * @param  {number} newTop slider's new top
+   * @return null
+   */
   function updateSlider(newTop) {
     dom.css(i.sliderY.element, 'top', newTop);
   }
 
+  /**
+   * updateSliderYGeometry
+   * @param  {number} newTop slider's new top
+   * @return null
+   */
   function updateSliderYGeometry(newTop) {
     i.sliderY.deltaY = 0;
     i.sliderY.top = newTop;
   }
 
-  function clickRailY(e) {
+  /**
+   * clickRailYHandler
+   * @param  {object} e  evnet
+   * @return null
+   */
+  function clickRailYHandler(e) {
     let originTop = dom.css(i.sliderY.element, 'top');
-    let newTop = getNewTop(e.layerY);
+    let newTop = getNewTop(e.offsetY);
+
     updateSlider(newTop);
-    updateBox(newTop, originTop);
+    updateContent(newTop, originTop);
     updateSliderYGeometry(newTop);
+
     e.preventDefault();
   }
 
-  event.bind(i.railY.element, 'click', clickRailY);
+  event.bind(i.railY.element, 'click', clickRailYHandler);
 };
