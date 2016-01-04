d52a9909f8fbe96b67ec9c1558bc4efbae2e4757
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-01-04 16:12:45 +0800

    rm index.js

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ let niceBar = {init: init};
 (function(factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
-  } else if (window && typeof window === 'object') {
+  } else if (typeof window === 'object') {
     window.niceBar = factory();
   } else if (typeof module === 'object' && module.exports) {
     module.exports = factory();
